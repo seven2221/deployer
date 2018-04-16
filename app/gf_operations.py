b8c7093e6b7a005b327cd083b5010d8823258896
IOnoshko <i.v.onoshko@yandex.ru>
2018-04-16 19:33:57 +0300

    изменения логики работы с гф

--- app/gf_operations.py
+++ app/gf_operations.py
@@ -9,15 +9,26 @@ from app.config import Config
  list-jbi-service-assemblies
  update-jbi-application-variable
  create-jbi-application-variable
+ 
+ asadmin create-jbi-application-variable --host ms-glass004 --port 4848 --user admin --passwordfile D:\Glassfish22\passfile123 --component sun-bpel-engine test_variable=testtest
+ asadmin create-jbi-application-variable --host hostname --port 4848 --user admin --passwordfile D:\Glassfish22\passfile --component sun-bpel-engine SubscriberSMConfigFileDir=D:\\Glassfish22\\domains\\domain1\\config\\SubscriberManagementService\\
+ asadmin shut-down-jbi-service-assembly --user admin --host ms-glass010 --port 4848 --passwordfile D:\GlassFish22\passfile SharedDataWeb
  '''
-# asadmin create-jbi-application-variable --host ms-glass004 --port 4848 --user admin --passwordfile D:\Glassfish22\passfile123 --component sun-bpel-engine test_variable=testtest
-# asadmin create-jbi-application-variable --host hostname --port 4848 --user admin --passwordfile D:\Glassfish22\passfile --component sun-bpel-engine SubscriberSMConfigFileDir=D:\\Glassfish22\\domains\\domain1\\config\\SubscriberManagementService\\
-# asadmin shut-down-jbi-service-assembly --user admin --host ms-glass010 --port 4848 --passwordfile D:\GlassFish22\passfile SharedDataWeb
+
 
 def check_SA(host, port):
     SAs = []
     passfile = Config.passfile
-    asadmin_command = subprocess.Popen(["asadmin", "list-jbi-service-assemblies", "--host", host, "--port", port, "--user", "admin", "--passwordfile", passfile], stdout=subprocess.PIPE, shell=True)
+    asadmin_command = subprocess.Popen\
+        (
+            [
+                "asadmin", "list-jbi-service-assemblies",
+                "--host", host,
+                "--port", port,
+                "--user", "admin",
+                "--passwordfile", passfile
+            ], stdout=subprocess.PIPE, shell=True
+        )
     for line in asadmin_command.stdout:
         if "executed successfully." not in str(line):
             SA = str(line).replace("b\'", "").replace("\\r\\n\'", "")
@@ -28,7 +39,18 @@ def check_SA(host, port):
 def undeploy_SA(host, port, SA):
     out = []
     passfile = Config.passfile
-    asadmin_command = subprocess.Popen(["asadmin", "undeploy-jbi-service-assembly", "--host", host, "--port", port, "--user", "admin", "--passwordfile", passfile, "--target", SA], stdout=subprocess.PIPE, shell=True)
+    asadmin_command = subprocess.Popen\
+        (
+            [
+                "asadmin", "undeploy-jbi-service-assembly",
+                "--host", host,
+                "--port", port,
+                "--user", "admin",
+                "--passwordfile", passfile,
+                "--target", SA
+            ], stdout=subprocess.PIPE, shell=True
+        )
+
 
 def deploy_SA(host, port, zip):
     out = []
@@ -38,7 +60,17 @@ def get_all_variables(host, port):
     exists_variables = []
     passfile = Config.passfile
     for component in Config.GFcomponents:
-        asadmin_command = subprocess.Popen(["asadmin", "list-jbi-application-variables", "--host", host, "--port", port, "--user", "admin", "--passwordfile", passfile, "--component", component], stdout=subprocess.PIPE, shell=True)
+        asadmin_command = subprocess.Popen\
+            (
+                [
+                    "asadmin", "list-jbi-application-variables",
+                    "--host", host,
+                    "--port", port,
+                    "--user", "admin",
+                    "--passwordfile", passfile,
+                    "--component", component
+                ], stdout=subprocess.PIPE, shell=True
+            )
         for line in asadmin_command.stdout:
             if "executed successfully." not in str(line) and "Nothing to list" not in str(line):
                 variable = str(line).split("=")[0].replace("b\'", "").replace("b\"", "").replace(" ", "")
@@ -49,7 +81,18 @@ def get_all_variables(host, port):
 
 def update_variable(host, port, component, variable, value):
     passfile = Config.passfile
-    asadmin_command = subprocess.Popen(["asadmin", "update-jbi-application-variable", "--host", host, "--port", port, "--user", "admin", "--passwordfile", passfile, "--component", component, variable, "=", value], stdout=subprocess.PIPE, shell=True)
+    asadmin_command = subprocess.Popen\
+        (
+            [
+                "asadmin", "update-jbi-application-variable",
+                "--host", host,
+                "--port", port,
+                "--user", "admin",
+                "--passwordfile", passfile,
+                "--component", component,
+                variable + "=" + value
+            ], stdout=subprocess.PIPE, shell=True
+        )
     for line in asadmin_command.stdout:
         if "executed successfully" in str(line):
             return "Success"
@@ -59,7 +102,18 @@ def update_variable(host, port, component, variable, value):
 
 def create_variable(host, port, component, variable, value):
     passfile = Config.passfile
-    asadmin_command = subprocess.Popen(["asadmin", "create-jbi-application-variable", "--host", host, "--port", port, "--user", "admin", "--passwordfile", passfile, "--component", component, variable + "=" + value], stdout=subprocess.PIPE, shell=True)
+    asadmin_command = subprocess.Popen\
+        (
+            [
+                "asadmin", "create-jbi-application-variable",
+                "--host", host,
+                "--port", port,
+                "--user", "admin",
+                "--passwordfile", passfile,
+                "--component", component,
+                variable + "=" + value
+            ], stdout=subprocess.PIPE, shell=True
+        )
     for line in asadmin_command.stdout:
         if "executed successfully" in str(line):
             return "Success"
